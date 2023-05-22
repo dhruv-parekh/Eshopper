fce80060ebaa5b87dd57cbbf520d8c6b260fa03e
dhruvparekh-admin <contact.dparekh@gmail.com>
2023-05-21 22:47:52 -0400

    test commit

--- src/main/java/com/dare/eshop/service/ShopService.java
+++ src/main/java/com/dare/eshop/service/ShopService.java
@@ -21,10 +21,10 @@ public class ShopService {
     public List<Product> filterProducts(String title) {
 
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product(1,"game",120,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBxaFNEFZrcA3vNdKIIjYxPMXIImwZiIoN3Q&usqp=CAU"));
-        productList.add(new Product(2,"movie",12,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLp7HepZrPccmlBuhEoXVAuDa4-PYJQs9y2A&usqp=CAU"));
-        productList.add(new Product(3,"car",12000,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEYPKdn1ad34Ok5X20cFbktRNGDddYky--kw&usqp=CAU"));
-        productList.add(new Product(4,"game",120,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5fg1ewYohWmpc7Wg7_gNkzTGgjjecnb3pA&usqp=CAU"));
+        productList.add(new Product(1,"game",120,"img/cat-4.jpg"));
+        productList.add(new Product(2,"movie",12,"img/cat-4.jpg"));
+        productList.add(new Product(3,"car",12000,"img/cat-4.jpg"));
+        productList.add(new Product(4,"game",120,"img/cat-4.jpg"));
 
         List<Product> filteredProducts = new ArrayList<>();
         for(Product p :productList){
