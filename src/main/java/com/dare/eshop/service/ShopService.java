fa58512188d866d3aa3b221f861614805a3eaa65
dhruvparekh-admin <contact.dparekh@gmail.com>
2023-05-23 15:40:12 -0400

    image view from remote source

--- src/main/java/com/dare/eshop/service/ShopService.java
+++ src/main/java/com/dare/eshop/service/ShopService.java
@@ -10,7 +10,8 @@ import java.util.List;
 public class ShopService {
     public List<Product> getAllProducts() {
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product(1,"game",120,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBxaFNEFZrcA3vNdKIIjYxPMXIImwZiIoN3Q&usqp=CAU"));
+
+        productList.add(new Product(1,"game",120,"https://yt3.googleusercontent.com/447XAt80-ow7poHghoEV2f_0xf-QypVPzTqEFlwVsvijrS56-dpTU3ICge4OP48V8nfk6x5j"));
         productList.add(new Product(2,"movie",12,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLp7HepZrPccmlBuhEoXVAuDa4-PYJQs9y2A&usqp=CAU"));
         productList.add(new Product(3,"car",12000,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEYPKdn1ad34Ok5X20cFbktRNGDddYky--kw&usqp=CAU"));
         productList.add(new Product(4,"game",120,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5fg1ewYohWmpc7Wg7_gNkzTGgjjecnb3pA&usqp=CAU"));
@@ -19,12 +20,12 @@ public class ShopService {
     }
 
     public List<Product> filterProducts(String title) {
-
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product(1,"game",120,"img/cat-4.jpg"));
-        productList.add(new Product(2,"movie",12,"img/cat-4.jpg"));
-        productList.add(new Product(3,"car",12000,"img/cat-4.jpg"));
-        productList.add(new Product(4,"game",120,"img/cat-4.jpg"));
+        productList.add(new Product(1,"game",120,"https://yt3.googleusercontent.com/447XAt80-ow7poHghoEV2f_0xf-QypVPzTqEFlwVsvijrS56-dpTU3ICge4OP48V8nfk6x5j"));
+        productList.add(new Product(2,"movie",12,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLp7HepZrPccmlBuhEoXVAuDa4-PYJQs9y2A&usqp=CAU"));
+        productList.add(new Product(3,"car",12000,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEYPKdn1ad34Ok5X20cFbktRNGDddYky--kw&usqp=CAU"));
+        productList.add(new Product(4,"game",120,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5fg1ewYohWmpc7Wg7_gNkzTGgjjecnb3pA&usqp=CAU"));
+
 
         List<Product> filteredProducts = new ArrayList<>();
         for(Product p :productList){
